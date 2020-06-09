dde1a86fa5bb878f615ec141062e9e876249e8c8
Adhavan M <adhavan@Adhavans-MacBook-Air.local>
2020-06-09 16:51:30 +0530

    test the flag

--- Q&A/test.php
+++ Q&A/test.php
@@ -1,4 +1,10 @@
 <?php
-echo $_POST['flag'];
-echo $_POST['index'];
-echo $_POST['token'];
+$flag = $_POST['flag'];
+$i = $_POST['index'];
+$token = $_POST['token'];
+$fstr = explode('}',explode('{',$flag)[1])[0];
+if(strcmp($fstr,md5('challenge'.$i.$token))==0){
+    echo "true";
+}else{
+    echo "false";
+}
